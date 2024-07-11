90a9dd88005fbdc3bc58693ebaa76acb831331e1
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2024-07-10 22:47:28 -0500

    microservicio 1 - repository

---
+++ gestion_usuarios/src/main/java/g4/sisvita/gestion_usuarios/service/AdministradorService.java
@@ -0,0 +1,31 @@
+package g4.sisvita.gestion_usuarios.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import g4.sisvita.gestion_usuarios.model.Administrador;
+
+@Service
+public class AdministradorService {
+    
+    @Autowired
+    private AdministradorRepository administradorRepository;
+
+    public List<Administrador> getAdministradores() {
+        return administradorRepository.findAll();
+    }
+
+    public Administrador saveAdministrador(Administrador administrador) {
+        return administradorRepository.save(administrador);
+    }
+
+    public Administrador getAdministradorById(int id) {
+        return administradorRepository.findById(id).orElse(null);
+    }
+
+    public void deleteAdministrador(int id) {
+        administradorRepository.deleteById(id);
+    }
+}
