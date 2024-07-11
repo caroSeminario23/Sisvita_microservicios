019d2a9d71b645512a1ce3d2c2e0fcfab3837e8e
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2024-07-11 00:41:53 -0500

    version 1.2.0

---
+++ gestion_evaluaciones/src/test/java/g4/sisvita/gestion_evaluaciones/GestionEvaluacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package g4.sisvita.gestion_evaluaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEvaluacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
